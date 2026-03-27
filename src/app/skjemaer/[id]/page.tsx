b37522d5a2879d49007d7ef5f3f33e8a795a0f03
haarogmag-cmyk <haarogmag@gmail.com>
2026-03-27 16:42:14 +0100

    Update page.tsx

--- src/app/skjemaer/[id]/page.tsx
+++ src/app/skjemaer/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { createServerSupabaseClient } from '@/lib/supabase'
+import { createClient } from '@/lib/supabase'
 import { notFound } from 'next/navigation'
 import FormResponseClient from './FormResponseClient'
 
